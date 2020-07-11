6734db6c28fe2cce56b5f7439cd58fe9289216a5
Arta Farahmand <arta.farahmand@gmail.com>
2020-07-11 17:03:11 -0600

    new sample code for Module 2

---
+++ DATA9401 – Module II/DATA9401–Sample Code–module2.sql
@@ -0,0 +1,35 @@
+SELECT * FROM nyairbnb_results
+
+-- Code to create a stored procedure --
+
+CREATE PROCEDURE sp_location (VARCHAR (200), VARCHAR (200), NUMERIC(9,6), NUMERIC(9,6))
+LANGUAGE plpgsql AS $$
+BEGIN
+	CREATE TABLE manhattonneighbourhood AS
+	SELECT host_name, neighbourhood, latitude, longitude 
+	FROM nyairbnb_results
+	WHERE neighbourhood = 'Manhattan';
+	
+	COMMIT;
+END;
+$$;
+
+CREATE PROCEDURE sp_nyLocation (VARCHAR (200), VARCHAR (200), NUMERIC(9,6), NUMERIC(9,6))
+LANGUAGE plpgsql AS $$
+BEGIN
+	CREATE TABLE brooklynneighbourhood AS
+	SELECT host_name, neighbourhood, latitude, longitude 
+	FROM nyairbnb_results
+	WHERE neighbourhood = 'Brooklyn';
+	
+	COMMIT;
+END;
+$$;
+
+-- Code to Execute a stored procedure --
+CALL sp_location('Jennifer', 'Manhattan', 40.647490, -73.972370 );
+CALL sp_nyLocation('LisaRoxanne', 'Brooklyn', 40.747670, -73.975000 );
+
+-- Select statements to check if the stored procedure executed correctly --
+SELECT * FROM manhattonneighbourhood;
+SELECT * FROM brooklynneighbourhood;
