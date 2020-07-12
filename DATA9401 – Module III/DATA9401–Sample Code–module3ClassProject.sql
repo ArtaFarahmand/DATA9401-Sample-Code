014c446cfa48ab6c0a845740a5af3921bba76d93
Arta Farahmand <arta.farahmand@gmail.com>
2020-07-12 10:38:13 -0600

    Module 3 - Updated the code organization for readability

--- DATA9401 – Module III/DATA9401–Sample Code–module3ClassProject.sql
+++ DATA9401 – Module III/DATA9401–Sample Code–module3ClassProject.sql
@@ -1,8 +1,27 @@
+-- Code to create DB tables --
 CREATE TABLE NYAIRBNB (
 	host_ID INT NOT NULL,
 	host_Name VARCHAR(200),
 	Neighbourhood_group VARCHAR(200)
 );
+
+CREATE TABLE NYAIRBNB_LOCATION (
+	host_ID INT NOT NULL,
+	Neighbourhood VARCHAR(200),
+	Latitude NUMERIC(2,5) NOT NULL,
+	Longitude NUMERIC(2,5) NOT NULL
+);
+
+CREATE TABLE NYAIRBNB_RESULTS (
+	host_ID INT NOT NULL,
+	host_Name VARCHAR(200),
+	Neighbourhood_group VARCHAR(200),
+	Neighbourhood VARCHAR(200),
+	Latitude DECIMAL(2,2) NOT NULL,
+	Longitude DECIMAL(2,2) NOT NULL
+);
+
+-- Code to Insert data into table --
 
 INSERT INTO NYAIRBNB VALUES (
 	2787,
@@ -24,12 +43,7 @@ INSERT INTO NYAIRBNB VALUES (
 
 SELECT * FROM NYAIRBNB;
 
-CREATE TABLE NYAIRBNB_LOCATION (
-	host_ID INT NOT NULL,
-	Neighbourhood VARCHAR(200),
-	Latitude NUMERIC(2,5) NOT NULL,
-	Longitude NUMERIC(2,5) NOT NULL
-);
+
 
 INSERT INTO NYAIRBNB_LOCATION VALUES (
 	2787,
@@ -54,6 +68,47 @@ INSERT INTO NYAIRBNB_LOCATION VALUES (
 	40.80902,
 	-73.9419
 );
+
+INSERT INTO NYAIRBNB VALUES (
+	4869,
+	'LisaRoxanne',
+	'Brooklyn'
+);
+
+INSERT INTO NYAIRBNB VALUES (
+	7192,
+	'Laura',
+	'Manhattan'
+);
+
+INSERT INTO NYAIRBNB VALUES (
+	7322,
+	'Chris',
+	'Manhattan'
+);
+
+INSERT INTO NYAIRBNB_LOCATION VALUES (
+	4869,
+	'Clinton Hill',
+	40.68514,
+	-73.95976
+);
+
+INSERT INTO NYAIRBNB_LOCATION VALUES (
+	7192,
+	'East Harlem',
+	40.79851,
+	-73.94399
+);
+
+INSERT INTO NYAIRBNB_LOCATION VALUES (
+	7322,
+	'Murray Hill',
+	40.74767,
+	-73.975
+);
+
+-- Code to Join Tables Togather --
 
 SELECT NYAIRBNB.*, NYAIRBNB_Location.*
 FROM NYAIRBNB INNER JOIN NYAIRBNB_Location ON NYAIRBNB.host_ID = NYAIRBNB_Location.host_ID;
@@ -81,41 +136,17 @@ SELECT NYAIRBNB.*, NYAIRBNB_Location.*
 FROM NYAIRBNB FULL OUTER JOIN NYAIRBNB_Location ON NYAIRBNB.host_ID = NYAIRBNB_Location.host_ID
 WHERE neighbourhood = 'Harlem';
 
-CREATE TABLE NYAIRBNB_RESULTS (
-	host_ID INT NOT NULL,
-	host_Name VARCHAR(200),
-	Neighbourhood_group VARCHAR(200),
-	Neighbourhood VARCHAR(200),
-	Latitude DECIMAL(2,2) NOT NULL,
-	Longitude DECIMAL(2,2) NOT NULL
-);
-
 INSERT INTO NYAIRBNB_RESULTS SELECT NYAIRBNB.host_ID, NYAIRBNB.host_Name, 
 NYAIRBNB.Neighbourhood_group,  NYAIRBNB_Location.Neighbourhood, NYAIRBNB_Location.Latitude, NYAIRBNB_Location.Longitude
 FROM NYAIRBNB RIGHT JOIN NYAIRBNB_Location 
 ON NYAIRBNB.host_ID = NYAIRBNB_Location.host_ID
 ORDER BY host_name
 
+-- Code to alter table data types --
+
 ALTER TABLE NYAIRBNB_RESULTS 
 ALTER COLUMN Longitude  TYPE DECIMAL(5,2);
 
-INSERT INTO NYAIRBNB VALUES (
-	4869,
-	'LisaRoxanne',
-	'Brooklyn'
-);
-
-INSERT INTO NYAIRBNB VALUES (
-	7192,
-	'Laura',
-	'Manhattan'
-);
-
-INSERT INTO NYAIRBNB VALUES (
-	7322,
-	'Chris',
-	'Manhattan'
-);
 
 ALTER TABLE NYAIRBNB_LOCATION 
 ALTER COLUMN Latitude  TYPE DECIMAL(5,2)
@@ -123,34 +154,13 @@ ALTER COLUMN Latitude  TYPE DECIMAL(5,2)
 ALTER TABLE NYAIRBNB_LOCATION 
 ALTER COLUMN Longitude  TYPE DECIMAL(5,2)
 
-INSERT INTO NYAIRBNB_LOCATION VALUES (
-	4869,
-	'Clinton Hill',
-	40.68514,
-	-73.95976
-);
-
-INSERT INTO NYAIRBNB_LOCATION VALUES (
-	7192,
-	'East Harlem',
-	40.79851,
-	-73.94399
-);
-
-INSERT INTO NYAIRBNB_LOCATION VALUES (
-	7322,
-	'Murray Hill',
-	40.74767,
-	-73.975
-);
-
 ALTER TABLE nyairbnb_results 
 ALTER COLUMN Latitude  TYPE Decimal(9,6);
 
 ALTER TABLE nyairbnb_results 
 ALTER COLUMN Longitude  TYPE Decimal(9,6);
 
-SELECT * FROM nyairbnb_results;
+-- Code to update tables --
 
 UPDATE nyairbnb_results
 SET latitude = 40.64749, longitude = -73.97237
@@ -175,6 +185,10 @@ WHERE host_name = 'Laura';
 UPDATE nyairbnb_results
 SET latitude = 40.74767, longitude = -73.975
 WHERE host_name = 'Chris';
+
+-- Code to check the results of operations --
+
+SELECT * FROM nyairbnb_results;
 
 SELECT * FROM nyairbnb_results 
 WHERE neighbourhood_group = 'Manhattan'
